285bcc9f89a0701a795c2d81c7754c08e619d33c
Jen Fong-Adwent <jen@ednapiranha.com>
2012-04-12 10:16:37 -0400

    renamed callback errors, added comments, fixed port settings

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 module.exports = function(app) {
+  /* Filter for checking if a user is authenticated before
+   * they can access views that require login
+   */
   var isAuthenticated = function(req, res, next) {
     if(!req.session.email) {
       res.redirect('/');
