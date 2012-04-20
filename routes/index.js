d9536f211feac99b5a94fa0793be020c43f3f612
Jen Fong-Adwent <jen@ednapiranha.com>
2012-04-20 13:01:16 -0400

    force no cache on homepage

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ module.exports = function(app) {
 
   // Home/main
   app.get('/', function(req, res) {
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     res.render('index', { title: 'Persona.org' });
   });
 
